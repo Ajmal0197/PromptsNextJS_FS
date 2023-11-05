b2ac2db57ebae357fb3a3a0c58122c375a2fcac4
Ajmal Hasan <ajmal0197@gmail.com>
2023-11-06 01:49:54 +0530

    Update page.jsx

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -6,6 +6,7 @@ import { useSearchParams } from "next/navigation";
 import Profile from "@components/Profile";
 
 const UserProfile = ({ params }) => {
+// router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   const searchParams = useSearchParams();
   const userName = searchParams.get("name");
 
